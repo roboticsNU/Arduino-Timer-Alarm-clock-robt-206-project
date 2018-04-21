1bd4339557abf6f32f4b467cd7ba5fa54c0388b9
MaximZ21 <38522419+MaximZ21@users.noreply.github.com>
2018-04-21 12:04:52 +0000

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,705 @@
+#define sd1t1 \
+digitalWrite(25,1);\
+digitalWrite(28,1);\
+digitalWrite(22,0);\
+digitalWrite(26,0);\
+digitalWrite(23,0);\
+digitalWrite(24,0);\
+digitalWrite(27,0);
+
+#define sd1t2 \
+digitalWrite(24,1);\
+digitalWrite(25,1);\
+digitalWrite(22,1);\
+digitalWrite(26,1);\
+digitalWrite(27,1);\
+digitalWrite(23,0);\
+digitalWrite(28,0);
+
+#define sd1t3 \
+digitalWrite(24,1);\
+digitalWrite(25,1);\
+digitalWrite(22,1);\
+digitalWrite(26,0);\
+digitalWrite(27,1);\
+digitalWrite(23,0);\
+digitalWrite(28,1);
+
+#define sd1t4 \
+digitalWrite(24,0);\
+digitalWrite(25,1);\
+digitalWrite(22,1);\
+digitalWrite(26,0);\
+digitalWrite(27,0);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+
+#define sd1t5 \
+digitalWrite(24,1);\
+digitalWrite(25,0);\
+digitalWrite(22,1);\
+digitalWrite(26,0);\
+digitalWrite(27,1);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+
+#define sd1t6 \
+digitalWrite(24,1);\
+digitalWrite(25,0);\
+digitalWrite(22,1);\
+digitalWrite(26,1);\
+digitalWrite(27,1);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+
+#define sd1t7 \
+digitalWrite(25,1);\
+digitalWrite(28,1);\
+digitalWrite(22,0);\
+digitalWrite(26,0);\
+digitalWrite(23,0);\
+digitalWrite(24,1);\
+digitalWrite(27,0);
+
+#define sd1t8 \
+digitalWrite(24,1);\
+digitalWrite(25,1);\
+digitalWrite(22,1);\
+digitalWrite(26,1);\
+digitalWrite(27,1);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+
+#define sd1t9 \
+digitalWrite(24,1);\
+digitalWrite(25,1);\
+digitalWrite(22,1);\
+digitalWrite(26,0);\
+digitalWrite(27,1);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+
+#define sd1t0 \
+digitalWrite(24,1);\
+digitalWrite(25,1);\
+digitalWrite(22,0);\
+digitalWrite(26,1);\
+digitalWrite(27,1);\
+digitalWrite(23,1);\
+digitalWrite(28,1);
+//1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+#define sd2t1 \
+digitalWrite(25+7,1);\
+digitalWrite(28+7,1);\
+digitalWrite(22+7,0);\
+digitalWrite(26+7,0);\
+digitalWrite(23+7,0);\
+digitalWrite(24+7,0);\
+digitalWrite(27+7,0);
+
+#define sd2t2 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,1);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,0);\
+digitalWrite(28+7,0);
+
+#define sd2t3 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,0);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,0);\
+digitalWrite(28+7,1);
+
+#define sd2t4 \
+digitalWrite(24+7,0);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,0);\
+digitalWrite(27+7,0);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+#define sd2t5 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,0);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,0);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+#define sd2t6 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,0);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,1);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+#define sd2t7 \
+digitalWrite(25+7,1);\
+digitalWrite(28+7,1);\
+digitalWrite(22+7,0);\
+digitalWrite(26+7,0);\
+digitalWrite(23+7,0);\
+digitalWrite(24+7,1);\
+digitalWrite(27+7,0);
+
+#define sd2t8 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,1);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+#define sd2t9 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,0);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+#define sd2t0 \
+digitalWrite(24+7,1);\
+digitalWrite(25+7,1);\
+digitalWrite(22+7,0);\
+digitalWrite(26+7,1);\
+digitalWrite(27+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(28+7,1);
+
+//22222222222222222222222222222222222222222222222222222222222222222
+#define sd3t1 \
+digitalWrite(25+14,1);\
+digitalWrite(28+14,1);\
+digitalWrite(22+14,0);\
+digitalWrite(26+14,0);\
+digitalWrite(23+14,0);\
+digitalWrite(24+14,0);\
+digitalWrite(27+14,0);
+
+#define sd3t2 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,1);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,0);\
+digitalWrite(28+14,0);
+
+#define sd3t3 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,0);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,0);\
+digitalWrite(28+14,1);
+
+#define sd3t4 \
+digitalWrite(24+14,0);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,0);\
+digitalWrite(27+14,0);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+#define sd3t5 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,0);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,0);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+#define sd3t6 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,0);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,1);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+#define sd3t7 \
+digitalWrite(25+14,1);\
+digitalWrite(28+14,1);\
+digitalWrite(22+14,0);\
+digitalWrite(26+14,0);\
+digitalWrite(23+14,0);\
+digitalWrite(24+14,1);\
+digitalWrite(27+14,0);
+
+#define sd3t8 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,1);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+#define sd3t9 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,1);\
+digitalWrite(26+14,0);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+#define sd3t0 \
+digitalWrite(24+14,1);\
+digitalWrite(25+14,1);\
+digitalWrite(22+14,0);\
+digitalWrite(26+14,1);\
+digitalWrite(27+14,1);\
+digitalWrite(23+14,1);\
+digitalWrite(28+14,1);
+
+//333333333333333333333333333333333333333333333333333
+#define sd4t1 \
+digitalWrite(25+24,1);\
+digitalWrite(28+24,1);\
+digitalWrite(22+24,0);\
+digitalWrite(26+24,0);\
+digitalWrite(23+24,0);\
+digitalWrite(24+24,0);\
+digitalWrite(27+24,0);
+
+#define sd4t2 \
+digitalWrite(24+24,1);\
+digitalWrite(25+24,1);\
+digitalWrite(22+24,1);\
+digitalWrite(26+24,1);\
+digitalWrite(27+24,1);\
+digitalWrite(23+24,0);\
+digitalWrite(28+24,0);
+
+#define sd4t3 \
+digitalWrite(24+24,1);\
+digitalWrite(25+24,1);\
+digitalWrite(22+24,1);\
+digitalWrite(26+24,0);\
+digitalWrite(27+24,1);\
+digitalWrite(23+24,0);\
+digitalWrite(28+24,1);
+
+#define sd4t4 \
+digitalWrite(24+24,0);\
+digitalWrite(25+24,1);\
+digitalWrite(22+24,1);\
+digitalWrite(26+24,0);\
+digitalWrite(27+24,0);\
+digitalWrite(23+24,1);\
+digitalWrite(28+24,1);
+#define sd4t5 \
+digitalWrite(24+21+3,1);\
+digitalWrite(25+21+3,0);\
+digitalWrite(22+21+3,1);\
+digitalWrite(26+21+3,0);\
+digitalWrite(27+21+3,1);\
+digitalWrite(23+21+3,1);\
+digitalWrite(28+21+3,1);
+
+#define sd4t6 \
+digitalWrite(24+24,1);\
+digitalWrite(25+24,0);\
+digitalWrite(22+24,1);\
+digitalWrite(26+24,1);\
+digitalWrite(27+24,1);\
+digitalWrite(23+24,1);\
+digitalWrite(28+24,1);
+
+#define sd4t7 \
+digitalWrite(25+21+3,1);\
+digitalWrite(28+21+3,1);\
+digitalWrite(22+21+3,0);\
+digitalWrite(26+21+3,0);\
+digitalWrite(23+21+3,0);\
+digitalWrite(24+21+3,1);\
+digitalWrite(27+21+3,0);
+
+#define sd4t8 \
+digitalWrite(24+21+3,1);\
+digitalWrite(25+21+3,1);\
+digitalWrite(22+21+3,1);\
+digitalWrite(26+21+3,1);\
+digitalWrite(27+21+3,1);\
+digitalWrite(23+21+3,1);\
+digitalWrite(28+21+3,1);
+
+#define sd4t9 \
+digitalWrite(24+21+3,1);\
+digitalWrite(25+21+3,1);\
+digitalWrite(22+21+3,1);\
+digitalWrite(26+21+3,0);\
+digitalWrite(27+21+3,1);\
+digitalWrite(23+21+3,1);\
+digitalWrite(28+21+3,1);
+
+#define sd4t0 \
+digitalWrite(24+21+3,1);\
+digitalWrite(25+21+3,1);\
+digitalWrite(22+21+3,0);\
+digitalWrite(26+21+3,1);\
+digitalWrite(27+21+3,1);\
+digitalWrite(23+21+3,1);\
+digitalWrite(28+21+3,1);
+
+//444444444444444444444444444444444444444444444
+//END OF DEFINE/////////////////////////////////////////////////////////////////////////////////////////
+
+//Global Variables
+unsigned long startTime;
+unsigned long currentTime;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+int setupButtonState;
+int lastSetupButtonState = LOW;
+int mode = 1; //1:setup //2:countdown //3:ringing //4:setting tasks //5:answering tasks
+int secondsButtonState;
+int minuteButtonState;
+int lastSecondsButtonState;
+int lastMinuteButtonState;
+int buttonPin = 2;
+int buttonPin2 = 3;
+int seconds;
+int minutes;
+int d1;
+int d2;
+int d3;
+int d4;
+int alltime;
+unsigned long time1;
+int rtime;
+int t;
+int ans;
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int var = digitalRead(3);
+  //Serial.print(var);
+  //Serial.print("\n");
+currentTime = millis();
+
+
+ int reading = digitalRead(2);
+  if (reading != lastSetupButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != setupButtonState) {
+      setupButtonState = reading;
+      if (setupButtonState == 1 && mode == 1) {
+        
+        mode = 2;
+        alltime = seconds+(minutes*60);
+        startTime = millis();
+        
+      }else if(setupButtonState == 1 && mode == 2){
+        mode = 1;
+      }else if(setupButtonState == 1 && mode == 3){
+        mode = 4;
+      }else if(setupButtonState == 1 && mode == 5 && d4==ans){
+        mode = 1;
+      }
+    }
+  }
+  lastSetupButtonState = reading;
+ if(mode == 5){
+  int reading1 = digitalRead(3);
+  if (reading1 != lastSecondsButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading1 != secondsButtonState) {
+      secondsButtonState = reading1;
+      
+  
+      if(secondsButtonState == HIGH){
+        d4++;
+        if(d4>9){
+          d4 = 0;
+        }
+      }
+    }
+  }
+  lastSecondsButtonState = reading1;
+ }
+  
+  if(mode == 1){
+     int reading1 = digitalRead(3);
+  if (reading1 != lastSecondsButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading1 != secondsButtonState) {
+      secondsButtonState = reading1;
+      
+  
+      if(secondsButtonState == HIGH){
+        seconds++;
+        if(seconds>59){
+          seconds = 0;
+        }
+      }
+    }
+  }
+  lastSecondsButtonState = reading1;
+
+  
+     int reading2 = digitalRead(4);
+  if (reading2 != lastMinuteButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading2 != minuteButtonState) {
+      minuteButtonState = reading2;
+      
+  
+      if(minuteButtonState == HIGH){
+        minutes++;
+        if(minutes>59){
+          minutes = 0;
+        }
+      }
+    }
+  }
+  lastMinuteButtonState = reading2;
+
+  d3 = seconds/10;
+  d4 = seconds%10;
+  d2 = minutes%10;
+  d1 = minutes/10; 
+  
+  }
+
+
+  if(mode == 2){
+    
+    time1 = currentTime - startTime;
+    rtime = alltime - time1/1000;
+    
+    minutes = rtime/60;
+    seconds = rtime%60;
+    d4=seconds%10;
+    d3=seconds/10;
+    d1=minutes/10;
+    d2=minutes%10;
+  }
+  if(mode == 4){
+    d1 = rand()%9+1;
+    d2 = rand()%2;
+    if(d2==0){
+digitalWrite(25+7,0);\
+digitalWrite(28+7,0);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,0);\
+digitalWrite(23+7,0);\
+digitalWrite(24+7,0);\
+digitalWrite(27+7,0);
+d3 = rand()%d1;
+ans = d1-d3;
+    }else{
+      digitalWrite(25+7,1);\
+digitalWrite(28+7,0);\
+digitalWrite(22+7,1);\
+digitalWrite(26+7,1);\
+digitalWrite(23+7,1);\
+digitalWrite(24+7,1);\
+digitalWrite(27+7,0);
+d3 = rand()%(9-d1);
+ans = d1+d3;
+    }
+    mode = 5;
+  }
+  
+   Serial.print("mode: ");
+Serial.print(mode);
+Serial.print("\t");
+Serial.print("setbs: ");
+Serial.print(setupButtonState);
+Serial.print("\t");
+
+Serial.print("time: ");
+Serial.print(currentTime-startTime);
+Serial.print("\t");
+Serial.print("alltime: ");
+Serial.print(alltime);
+Serial.print("\t");
+Serial.print("minutes: ");
+Serial.print(minutes);
+Serial.print("\t");
+Serial.print("seconds: ");
+Serial.print(seconds);
+Serial.print("\n");
+switch(d4){
+    case 0:
+      sd4t0;
+      break;
+    case 1:
+      sd4t1;
+      break;
+    case 2:
+      sd4t2;
+      break;
+     case 3:
+      sd4t3;
+      break;
+    case 4:
+      sd4t4;
+      break;
+    case 5:
+      sd4t5;
+      break;
+    case 6:
+      sd4t6;
+      break;
+    case 7:
+      sd4t7;
+      break;
+    case 8:
+      sd4t8;
+      break;
+    case 9:
+      sd4t9;
+      break;
+  }
+  switch(d3){
+    case 0:
+      sd3t0;
+      break;
+    case 1:
+      sd3t1;
+      break;
+    case 2:
+      sd3t2;
+      break;
+     case 3:
+      sd3t3;
+      break;
+    case 4:
+      sd3t4;
+      break;
+    case 5:
+      sd3t5;
+      break;
+    case 6:
+      sd3t6;
+      break;
+    case 7:
+      sd3t7;
+      break;
+    case 8:
+      sd3t8;
+      break;
+    case 9:
+      sd3t9;
+      break;
+  }
+  if(mode!=4 && mode!=5){
+  switch(d2){
+    case 0:
+      sd2t0;
+      break;
+    case 1:
+      sd2t1;
+      break;
+    case 2:
+      sd2t2;
+      break;
+     case 3:
+      sd2t3;
+      break;
+    case 4:
+      sd2t4;
+      break;
+    case 5:
+      sd2t5;
+      break;
+    case 6:
+      sd2t6;
+      break;
+    case 7:
+      sd2t7;
+      break;
+    case 8:
+      sd2t8;
+      break;
+    case 9:
+      sd2t9;
+      break;
+  }
+  }
+  switch(d1){
+    case 0:
+      sd1t0;
+      break;
+    case 1:
+      sd1t1;
+      break;
+    case 2:
+      sd1t2;
+      break;
+     case 3:
+      sd1t3;
+      break;
+    case 4:
+      sd1t4;
+      break;
+    case 5:
+      sd1t5;
+      break;
+    case 6:
+      sd1t6;
+      break;
+    case 7:
+      sd1t7;
+      break;
+    case 8:
+      sd1t8;
+      break;
+    case 9:
+      sd1t9;
+      break;
+  }
+  if(rtime<=0 && mode == 2){
+    sd1t8;
+    sd2t8;
+    sd3t8;
+    sd4t8;
+    mode = 4;
+  }
+
+  if(mode == 3 || mode == 4 || mode == 5){
+    t+=35;
+    if(t>2000){
+      t = 100;
+    }
+    tone(5, t, 2000);
+  }
+  
+  // put your main code here, to run repeatedly:
+
+
+
+}
+
